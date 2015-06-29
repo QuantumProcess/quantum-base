38c538daedc8a886b6009c99cbd7a46ebd2662ec
MATIAS GALLEGO <matiasgallego@gmail.com>
2015-06-29 15:00:36 -0300

    Hiding groups option until get developed

--- client/app/modules/qgroups/config/qgroups.config.js
+++ client/app/modules/qgroups/config/qgroups.config.js
@@ -1,13 +1,13 @@
 'use strict';
 var app = angular.module('com.module.qgroups');
 
-app.run(function($rootScope, Qgroup, gettextCatalog) {
-  $rootScope.addMenu(gettextCatalog.getString('Groups'), 'app.qgroups.list',
-    'fa-users');
+app.run(function($stateParams, $rootScope, Qgroup, gettextCatalog) {
 
-  Qgroup.find(function(data) {
-    $rootScope.addDashboardBox(gettextCatalog.getString('Groups'),
-      'bg-yellow', 'ion-android-people', data.length, 'app.qgroups.list');
-  });
-
+  // $rootScope.addMenu(gettextCatalog.getString('Groups'), 'app.qgroups.list',
+  //   'fa-users', ['user']);
+  //
+  // Qgroup.find(function(data) {
+  //   $rootScope.addDashboardBox(gettextCatalog.getString('Groups'),
+  //     'bg-yellow', 'ion-android-people', data.length, 'app.qgroups.list', ['user']);
+  //   });
 });
